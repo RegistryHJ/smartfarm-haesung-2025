01887f7dd00552fea02b079ec1ce829c938ca592
RegistryHJ <registry.fordev@gmail.com>
2025-08-26 23:36:19 +0900

    comment: align ex04 comments

--- examples/ex04_neopixel.py
+++ examples/ex04_neopixel.py
@@ -5,4 +5,4 @@ from neopixel import NeoPixel
 np = NeoPixel(machine.Pin(21), 1)
 
 np[0] = (25, 25, 25)  # NeoPixel의 색상과 밝기 설정
-np.write()  # NeoPixel에 색상 적용
+np.write()            # NeoPixel에 색상 적용
