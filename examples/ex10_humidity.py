ddac66e23daedc2f0b9ea4786b62e6a37c21e999
RegistryHJ <registry.fordev@gmail.com>
2025-08-31 05:05:57 +0900

    feat: add ex10

---
+++ examples/ex10_humidity.py
@@ -0,0 +1,9 @@
+from machine import Pin, I2C
+from utime import sleep
+from aht21 import AHT21
+
+aht = AHT21(I2C(1, sda=Pin(14), scl=Pin(15), freq=400000))
+
+while True:
+  print(aht.read()[0])
+  sleep(1)
