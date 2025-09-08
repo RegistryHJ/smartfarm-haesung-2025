28158da89444e73e39d7b600494b3d7fab2adb55
RegistryHJ <registry.fordev@gmail.com>
2025-09-09 02:44:51 +0900

    comment: fix to use coordinates

--- examples/ex16_oled_point.py
+++ examples/ex16_oled_point.py
@@ -3,5 +3,5 @@ from ssd1306 import SSD1306_I2C
 
 oled = SSD1306_I2C(128, 64, SoftI2C(sda=Pin(4), scl=Pin(5)))
 
-oled.pixel(64, 32, 1)  # 화면 가운데에 점 찍기
+oled.pixel(64, 32, 1)  # (64, 32)에 점 찍기
 oled.show()
